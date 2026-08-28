08ae8fa0c9d202607b81445c3222de38138469f3
Maxime Nabli <72494139+MaximeNabli@users.noreply.github.com>
2026-08-28 13:50:30 +0200

    Comment out correctAnswer in Level8.tsx
    Comment out the correctAnswer assignment in Level8.tsx

--- src/levels/Level8.tsx
+++ src/levels/Level8.tsx
@@ -37,7 +37,7 @@ export default function Level8({ onUnlock }: Level8Props) {
         </>
       }
 
-      correctAnswer="Never Gonna Give You Up"
+      //correctAnswer="Never Gonna Give You Up"
       nextRoute="/odoo-intern-8"
       
       hints={[
